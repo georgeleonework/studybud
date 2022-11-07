e56973237a99294b62cf052283ce3e2d77f62aa9
georgeleonework <georgeleonework@gmail.com>
2022-11-07 11:23:07 -0500

    added form for delete operatopms

--- base/views.py
+++ base/views.py
@@ -27,3 +27,19 @@ def createRoom(request):
             return redirect('home') #this is the name value from our form which allows ur to reference easier
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk): #the pk here tells us which item were updating
+    room = Room.objects.get(id=pk) #this initializes the room we want to access by the pk
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room) #the instance value determines the room were updated
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+# def deleteRoom(request, pk):
+#     room = Room.objects.get(id=pk)
