3d1fd90cc8780baa1dfb234943ce61c8deb17aab
georgeleonework <georgeleonework@gmail.com>
2022-11-08 13:32:45 -0500

    set up static file dir in settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+#this lets django know there are actually static files to be processed
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
